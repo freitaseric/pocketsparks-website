54684df9e69eaf5ceea1682dad4662727ffc9f7a
Eric Freitas <ericfreitas371@gmail.com>
2025-02-12 21:38:21 -0400

    feat: add not found error handle page

--- src/app/components/views/not-found/not-found.component.ts
+++ src/app/components/views/not-found/not-found.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
 	selector: 'app-not-found',
-	imports: [],
+	imports: [RouterLink],
 	templateUrl: './not-found.component.html',
 	styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+	refresh() {
+		location.reload()
+	}
+}
